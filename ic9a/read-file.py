ab640f298b232a634d5f20605f0e57a201c611d1
Jose Torres <jtorresocc@gmail.com>
2022-10-25 11:28:32 -0700

    added in-class exercises 9a.

--- ic9a/read-file.py
+++ ic9a/read-file.py
@@ -1,11 +1,6 @@
 infile = open("input3.txt", "r")
 line = infile.readline()
 
-
-# while line != "":
-#     line = infile.readline()
-#     print(line)
-
 for line in infile:
     line = line.rsplit()
 
